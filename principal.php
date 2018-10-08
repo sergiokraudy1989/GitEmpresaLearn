3b1db36f6b6cdbd11ec51f4ce6ffd181cdead867
sergiokraudy1989 <parnasokraudy@yahoo.com>
2018-10-07 21:43:50 -0600

    Este es el primer commit

---
+++ principal.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pruebas con git</title>
+</head>
+<body>
+    <h1>hola esta es mi prueba de git</h1>
+
+    <p>linea1</p>
+    <p>linea2</p>
+    <p>linea3</p>
+</body>
+</html>
